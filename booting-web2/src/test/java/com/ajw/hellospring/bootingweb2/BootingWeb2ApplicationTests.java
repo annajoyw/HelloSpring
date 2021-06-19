3747417581d89e7c8bc45f48101c6973ee06fe01
annajoyw <arealjoyw@gmail.com>
2021-06-19 14:54:43 -0400

    Initial commit

---
+++ booting-web2/src/test/java/com/ajw/hellospring/bootingweb2/BootingWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajw.hellospring.bootingweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
